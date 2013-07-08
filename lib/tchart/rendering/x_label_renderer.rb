35483751344bcbb2aae4de4303392ca4eafd9813
Michael Lewandowski <milewgit@gmail.com>
2013-07-08 11:13:13 -0400

    Add XLabelRenderer.

---
+++ lib/tchart/rendering/x_label_renderer.rb
@@ -0,0 +1,22 @@
+module TChart
+  class XLabelRenderer
+    def initialize(settings, chart)
+      @settings = settings
+      @chart = chart
+    end
+    
+    def render(x_label)
+      <<-EOS.unindent.indent(4)
+        % #{x_label.date.year}
+        \\draw (#{f x_label.x_coordinate}mm, #{f @settings.x_label_y_coordinate}mm) node [xlabel] {#{x_label.date.year}};
+        \\draw [draw = black!5] (#{f x_label.x_coordinate}mm, #{f 0}mm) -- (#{f x_label.x_coordinate}mm, #{f @chart.y_length}mm);
+      EOS
+    end
+
+    # f(1.2345) => 1.23
+    # TODO: to be moved into base class or utilities module
+    def f(number) # => String
+      '%.02f' % number
+    end
+  end
+end
---
+++ test/tchart/rendering/x_label_renderer_test.rb
@@ -0,0 +1,17 @@
+require_relative '../../test_helper'
+
+module TChart
+  describe XLabelRenderer, "render" do
+    it "generates TeX code to render an x-axis label" do
+      settings = stub( x_label_y_coordinate: -3 )
+      chart = stub( y_length: 100 )
+      x_label = stub( date: Date.new(1985,1,1), x_coordinate: 20 )
+      renderer = XLabelRenderer.new(settings, chart)
+      renderer.render(x_label).must_equal <<-EOS.unindent.indent(4)
+        % 1985
+        \\draw (20.00mm, -3.00mm) node [xlabel] {1985};
+        \\draw [draw = black!5] (20.00mm, 0.00mm) -- (20.00mm, 100.00mm);
+      EOS
+    end
+  end
+end
