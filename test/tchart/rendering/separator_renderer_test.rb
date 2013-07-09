695b68870c73309d52a2f084f3fe3641afc53979
Michael Lewandowski <milewgit@gmail.com>
2013-07-09 15:32:55 -0400

    Add chart argument tp SeparatorRenderer#render.

--- test/tchart/rendering/separator_renderer_test.rb
+++ test/tchart/rendering/separator_renderer_test.rb
@@ -3,9 +3,10 @@ require_relative '../../test_helper'
 module TChart
   describe SeparatorRenderer, "render" do
     it "generates TeX code to render a separator" do
-      separator = stub( y_coordinate: 10, x_length: 20 )
+      chart = stub( x_length: 20 )
+      separator = stub( y_coordinate: 10 )
       renderer = SeparatorRenderer.new
-      renderer.render(separator).must_equal <<-EOS.unindent.indent(4)
+      renderer.render(chart, separator).must_equal <<-EOS.unindent.indent(4)
         % horizontal separator line
         \\draw [draw = black!5] (0.00mm, 10.00mm) -- (20.00mm, 10.00mm);
       EOS
