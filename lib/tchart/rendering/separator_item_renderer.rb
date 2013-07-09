aa27717c080d0a483db0aff8c76370f0fc9133a6
Michael Lewandowski <milewgit@gmail.com>
2013-07-09 15:35:26 -0400

    Rename SeparatorRenderer to SeparatorItemRenderer.

--- lib/tchart/rendering/separator_item_renderer.rb
+++ lib/tchart/rendering/separator_item_renderer.rb
@@ -1,10 +1,10 @@
 module TChart
-  class SeparatorRenderer
-    def render(chart, separator)
+  class SeparatorItemRenderer
+    def render(chart, separator_item)
       # TODO: the responsibility of indentation belongs somewhere else?
       <<-EOS.unindent.indent(4)
         % horizontal separator line
-        \\draw [draw = black!5] (#{f 0}mm, #{f separator.y_coordinate}mm) -- (#{f chart.x_length}mm, #{f separator.y_coordinate}mm);
+        \\draw [draw = black!5] (#{f 0}mm, #{f separator_item.y_coordinate}mm) -- (#{f chart.x_length}mm, #{f separator_item.y_coordinate}mm);
       EOS
     end
 
--- test/tchart/rendering/separator_item_renderer_test.rb
+++ test/tchart/rendering/separator_item_renderer_test.rb
@@ -1,11 +1,11 @@
 require_relative '../../test_helper'
 
 module TChart
-  describe SeparatorRenderer, "render" do
+  describe SeparatorItemRenderer, "render" do
     it "generates TeX code to render a separator" do
       chart = stub( x_length: 20 )
       separator = stub( y_coordinate: 10 )
-      renderer = SeparatorRenderer.new
+      renderer = SeparatorItemRenderer.new
       renderer.render(chart, separator).must_equal <<-EOS.unindent.indent(4)
         % horizontal separator line
         \\draw [draw = black!5] (0.00mm, 10.00mm) -- (20.00mm, 10.00mm);
