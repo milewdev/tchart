f976d6f704fa0bd88af593112a9aa366a5037777
Michael Lewandowski <milewgit@gmail.com>
2013-07-13 00:00:44 -0400

    Add Tex code generator class.

---
+++ lib/tchart/rendering/tex.rb
@@ -0,0 +1,56 @@
+module Kernel
+  def tex(output, &block)
+    TChart::Tex.tex(output, &block)
+  end
+end
+
+module TChart
+  class Tex
+    
+    def self.tex(output, &block)
+      Tex.new(output).instance_eval(&block)
+    end
+    
+    def initialize(output)
+      @output = output
+    end
+    
+    def comment(text)
+      @output << "% #{escape_tex_special_chars text}\n"
+    end
+    
+    def line(x1, y1, x2, y2)
+      @output << "\\draw [draw = black!5] (#{f x1}mm, #{f y1}mm) -- (#{f x2}mm, #{f y2}mm);\n"
+    end
+    
+    def label(x_mid, y, width, style, text)
+      #@output << "\\draw (#{f x}mm, #{f y}mm) node [#{style}] {#{text}};\n"
+      @output << "\\node [#{style}, text width = #{f width}mm] at (#{f x_mid}mm, #{f y}mm) {#{escape_tex_special_chars text}};\n"
+    end
+    
+    def bar(x1, x2, y, style)
+      x_mid, width = to_tikz_coords(x1, x2)
+      @output << "\\node [#{style}] at (#{f x_mid}mm, #{f y}mm) [minimum width = #{f width}mm] {};\n"
+    end
+    
+  private
+  
+    # f(1.2345) => 1.23
+    def f(number)
+      '%.02f' % number
+    end
+    
+    # escape_tex_special_chars("a#b&c") => "a\#b\&c"
+    def escape_tex_special_chars(text) # => String
+      text.gsub(/([#&])/, '\\\\\\1')
+    end
+    
+    # to_tikx_coords(x1, x2) => x_mid, width
+    def to_tikz_coords(x1, x2)
+      width = x2 - x1
+      x_mid = x1 + (width / 2.0)
+      [x_mid, width]
+    end
+  
+  end
+end
---
+++ test/tchart/rendering/tex_test.rb
@@ -0,0 +1,58 @@
+require_relative '../../test_helper'
+
+module TChart
+  describe Tex, "comment" do
+    it "generates a TeX comment" do
+      output = StringIO.new
+      tex output do
+        comment "this is a comment"
+      end
+      output.string.must_equal "% this is a comment\n"
+    end
+    it "escapes TeX special characters in comments" do
+      output = StringIO.new
+      tex output do
+        comment "this is a comment with TeX special characters: # &"
+      end
+      output.string.must_equal "% this is a comment with TeX special characters: \\# \\&\n"
+    end
+  end
+  
+  describe Tex, "line" do
+    it "generates a TikZ code for a line" do
+      output = StringIO.new
+      tex output do
+        line 10, 20, 30, 40   # x1, y1, x2, y2
+      end
+      output.string.must_equal "\\draw [draw = black!5] (10.00mm, 20.00mm) -- (30.00mm, 40.00mm);\n"
+    end
+  end
+  
+  describe Tex, "label" do
+    it "generates TikZ code for a chart label" do
+      output = StringIO.new
+      tex output do
+        label 10, 20, 15, 'some_style', 'the label text'  # x_mid, y, width, style, text
+      end
+      #output.string.must_equal "\\draw (10.00mm, 30.00mm) node [some_style] {the label text};\n"
+      output.string.must_equal "\\node [some_style, text width = 15.00mm] at (10.00mm, 20.00mm) {the label text};\n"
+    end
+    it "escapes TeX special characters in the label text" do
+      output = StringIO.new
+      tex output do
+        label 10, 20, 15, 'some_style', 'TeX special characters: # &'  # x_mid, y, width, style, text
+      end
+      output.string.must_equal "\\node [some_style, text width = 15.00mm] at (10.00mm, 20.00mm) {TeX special characters: \\# \\&};\n"
+    end
+  end
+  
+  describe Tex, "bar" do
+    it "generates TikZ code for a horizontal bar on the chart" do
+      output = StringIO.new
+      tex output do
+        bar 10, 40, 50, 'some_style'  # x1, x2, y, style
+      end
+      output.string.must_equal "\\node [some_style] at (25.00mm, 50.00mm) [minimum width = 30.00mm] {};\n"
+    end
+  end
+end
