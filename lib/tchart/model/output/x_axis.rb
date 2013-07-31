deb40ea490a91763ca341d585636cb102cdd45eb
Michael Lewandowski <milewgit@gmail.com>
2013-07-31 12:59:30 -0400

    Refactor: extract private methods.

--- lib/tchart/model/output/x_axis.rb
+++ lib/tchart/model/output/x_axis.rb
@@ -10,9 +10,19 @@ module TChart
     end
     
     def render(tex)
+      render_labels(tex)
+      render_gridlines(tex)
+    end
+    
+  private
+    
+    def render_labels(tex)
       tex.comment "x-axis labels"
       labels.each { |label| label.render(tex) }
       tex.newline
+    end
+    
+    def render_gridlines(tex)
       tex.comment "vertical grid lines"
       gridlines.each { |gridline| gridline.render(tex) }
       tex.newline
