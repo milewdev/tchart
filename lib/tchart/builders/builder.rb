1e30a261113a14714e91a16401f50762b278cbbd
Michael Lewandowski <milewgit@gmail.com>
2013-08-07 10:13:35 -0400

    Retire Builder#build_x_axis_labels.

--- lib/tchart/builders/builder.rb
+++ lib/tchart/builders/builder.rb
@@ -4,12 +4,6 @@ module TChart
     def self.build_x_items(layout)
       layout.x_axis_dates.map { |year| XItem.new(year) }
     end
-    
-    def self.build_x_axis_labels(layout)
-      layout.x_axis_dates
-        .zip(layout.x_axis_label_x_coordinates)
-        .map { |year, x| Label.new(xy(x, layout.x_label_y_coordinate), layout.x_label_width, "xlabel", year.to_s) }
-    end
-  
+      
   end
 end
