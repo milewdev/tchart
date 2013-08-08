d48db112685cb01f64fd49ab1d8f67a5b87a213b
milewgit <milewgit@gmail.com>
2013-08-07 20:18:08 -0400

    Minor code reformatting.

--- lib/tchart/ruby/kernel.rb
+++ lib/tchart/ruby/kernel.rb
@@ -1,7 +1,7 @@
 module Kernel
   
   #
-  # Syntactic sugar: xy(4,2) is shorthand for Coordinate.new(4, 2)
+  # Syntactic sugar: xy(4, 2) is shorthand for Coordinate.new(4, 2)
   #
   def xy(x, y)
     TChart::Coordinate.new(x, y)
