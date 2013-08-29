a9df4c389bafe74fd6ea21d491a8a563d9a4387e
Michael Lewandowski <milewgit@gmail.com>
2013-08-28 21:54:21 -0400

    Convert ChartBuilder from a class to a module.
    Simplified the code.

--- lib/tchart/process/chart_builder.rb
+++ lib/tchart/process/chart_builder.rb
@@ -1,62 +1,48 @@
 module TChart
-  class ChartBuilder
+  module ChartBuilder
     
     def self.build(layout, items) # => Chart
-      ChartBuilder.new(layout, items).build
-    end
-    
-  # private
-    
-    attr_reader :layout       # Dimensions of the label areas, the plot area, etc.
-    attr_reader :items        # The items being plotted.
-    attr_reader :elements     # Generated grid lines, labels, and bars that make up the chart.
-
-    def initialize(layout, items)
-      @items = items
-      @layout = layout
-      @elements = []
-    end
-  
-    def build # => Chart
-      build_frame
-      build_x_items
-      build_y_items
+      elements = []
+      elements.concat build_frame(layout)
+      elements.concat build_x_items(layout)
+      elements.concat build_y_items(layout, items)
       Chart.new(elements)
     end
     
   private
   
-    def build_frame
-      elements.push new_frame_top
-      elements.push new_frame_bottom
+    def self.build_frame(layout) # => [ element, ... ]
+      [] << new_frame_top(layout) << new_frame_bottom(layout)
     end
   
-    def new_frame_top # => GridLine
+    def self.new_frame_top(layout) # => GridLine
       GridLine.new(xy(0, layout.y_axis_length), xy(layout.x_axis_length, layout.y_axis_length))
     end
   
-    def new_frame_bottom # => GridLine
+    def self.new_frame_bottom(layout) # => GridLine
       GridLine.new(xy(0, 0), xy(layout.x_axis_length, 0))
     end
     
-    def build_x_items
-      layout.x_axis_tick_dates.zip(layout.x_axis_tick_x_coordinates).each do |date, x| 
-        @elements.push new_x_label(date.year, x)
-        @elements.push new_x_gridline(x)
+    def self.build_x_items(layout) # => [ element, ... ]
+      layout.x_axis_tick_dates.zip(layout.x_axis_tick_x_coordinates).inject( [] ) do |elements, date_zip_x|
+        date, x = date_zip_x
+        elements << new_x_label(layout, date.year, x) 
+        elements << new_x_gridline(layout, x)
       end
     end
     
-    def new_x_label(year, x) # => Label
+    def self.new_x_label(layout, year, x) # => Label
       Label.build_xlabel(xy(x, layout.x_axis_label_y_coordinate), layout.x_axis_label_width, year.to_s) 
     end
     
-    def new_x_gridline(x) # => GridLine
+    def self.new_x_gridline(layout, x) # => GridLine
       GridLine.new(xy(x, 0), xy(x, layout.y_axis_length))
     end
     
-    def build_y_items
-      items.zip(layout.y_axis_tick_y_coordinates).each do |item, y| 
-        @elements.concat item.build(@layout, y)
+    def self.build_y_items(layout, items) # => [ element, ... ]
+      items.zip(layout.y_axis_tick_y_coordinates).inject( [] ) do |elements, item_zip_y| 
+        item, y = item_zip_y
+        elements.concat item.build(layout, y)
       end
     end
       
