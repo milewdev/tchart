ccc02656d33b4c58bd835aa7aad810d6ebfd0a1d
Michael Lewandowski <milewgit@gmail.com>
2013-09-03 14:23:20 -0400

    Add TODO.

--- lib/tchart/ruby/kernel.rb
+++ lib/tchart/ruby/kernel.rb
@@ -3,6 +3,7 @@ module Kernel
   #
   # Syntactic sugar: xy(4, 2) is shorthand for Coordinate.new(4, 2)
   #
+  # TODO: move to coordinate.rb
   def xy(x, y) # => Coordinate
     TChart::Coordinate.new(x, y)
   end
