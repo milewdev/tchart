881d213c6c6a8fb5029e48a02e4f002dbcd91fae
Michael Lewandowski <milewgit@gmail.com>
2013-09-03 15:55:41 -0400

    Move Kernel#xy code into coordinate.rb.

--- lib/tchart/model/coordinate.rb
+++ lib/tchart/model/coordinate.rb
@@ -11,3 +11,15 @@ module TChart
     
   end
 end
+
+
+module Kernel
+  
+  #
+  # Syntactic sugar: xy(4, 2) is shorthand for Coordinate.new(4, 2)
+  #
+  def xy(x, y) # => Coordinate
+    TChart::Coordinate.new(x, y)
+  end
+
+end
