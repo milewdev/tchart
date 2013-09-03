90a0201f9d3e635f6f5356c388c48ed1b0ae58e6
Michael Lewandowski <milewgit@gmail.com>
2013-09-03 17:13:20 -0400

    Remove superfluous comment.

--- lib/tchart/model/coordinate.rb
+++ lib/tchart/model/coordinate.rb
@@ -14,12 +14,7 @@ end
 
 
 module Kernel
-  
-  #
-  # Syntactic sugar: xy(4, 2) is shorthand for Coordinate.new(4, 2)
-  #
   def xy(x, y) # => Coordinate
     TChart::Coordinate.new(x, y)
   end
-
 end
