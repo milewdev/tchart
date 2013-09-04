24c4800f2c42369c3af1681879113ce8da85ca48
Michael Lewandowski <milewgit@gmail.com>
2013-09-03 23:09:03 -0400

    Check that existing output file is actually a file.

--- lib/tchart/process/command_line_parser.rb
+++ lib/tchart/process/command_line_parser.rb
@@ -1,8 +1,6 @@
 module TChart
   module CommandLineParser
     
-    # TODO: handle tchart - output, tchart input -, and tchart - -
-    #       where - means stdin and/or stdout
     def self.parse(argv) # => CommandLineArgs, errors
       data_filename, tex_filename = argv
       errors = validate_args(argv, data_filename, tex_filename)
@@ -13,14 +11,15 @@ module TChart
   
     def self.validate_args(argv, data_filename, tex_filename)
       errors = []
-      case
-      when argv.length != 2
+      if argv.length != 2
         errors << "Usage: tchart input-data-filename output-tikz-filename"
-      when !File.exists?(data_filename)
+      elsif ! File.exists?(data_filename)
         errors << "Error: input data file \"#{data_filename}\" not found."
-      when !File.file?(data_filename)
+      elsif ! File.file?(data_filename)
         errors << "Error: input data file \"#{data_filename}\" is not a file."
-      when same_file?(data_filename, tex_filename)
+      elsif File.exists?(tex_filename) && ! File.file?(tex_filename)
+        errors << "Error: existing output data file \"#{tex_filename}\" is not a file."
+      elsif same_file?(data_filename, tex_filename)
         errors << "Error: input \"#{data_filename}\" and output \"#{tex_filename}\" refer to the same file."
       end
       errors
--- test/tchart/process/command_line_parser_test.rb
+++ test/tchart/process/command_line_parser_test.rb
@@ -5,37 +5,45 @@ module TChart
     it "returns CommandLineArgs with appropriate attributes parsed from the command line" do
       File.expects(:exists?).with('input_filename').returns(true)
       File.expects(:file?).with('input_filename').returns(true)
-      args, _ = CommandLineParser.parse( [ 'input_filename', 'tex_filename' ] )
+      File.expects(:exists?).with('output_filename').returns(false)
+      args, _ = CommandLineParser.parse( [ 'input_filename', 'output_filename' ] )
       args.data_filename.must_equal 'input_filename'
-      args.tex_filename.must_equal 'tex_filename'
+      args.tex_filename.must_equal 'output_filename'
     end
     it "returns an error if there are fewer than two arguments" do
-      _, errors = CommandLineParser.parse( [ 'arg1' ] )
+      _, errors = CommandLineParser.parse( [ 'input_filename' ] )
       errors.must_include "Usage: tchart input-data-filename output-tikz-filename"
     end
     it "returns an error if there are more than two arguments" do
-      _, errors = CommandLineParser.parse( [ 'arg1', 'arg2', 'arg3' ] )
+      _, errors = CommandLineParser.parse( [ 'input_filename', 'output_filename', 'arg3' ] )
       errors.must_include "Usage: tchart input-data-filename output-tikz-filename"
     end
     it "returns an error if the input and output file names are the same" do
-      File.expects(:exists?).with('arg1').returns(true)
-      File.expects(:file?).with('arg1').returns(true)
-      _, errors = CommandLineParser.parse( [ 'arg1', './arg1' ] )
-      errors.must_include "Error: input \"arg1\" and output \"./arg1\" refer to the same file."
+      File.expects(:exists?).with('input_filename').returns(true)
+      File.expects(:file?).with('input_filename').returns(true)
+      File.expects(:exists?).with('./input_filename').returns(true)
+      File.expects(:file?).with('./input_filename').returns(true)
+      _, errors = CommandLineParser.parse( [ 'input_filename', './input_filename' ] )
+      errors.must_include "Error: input \"input_filename\" and output \"./input_filename\" refer to the same file."
     end
     it "returns an error if the input data file does not exist" do
-      File.expects(:exists?).with('does_not_exist').returns(false)
-      _, errors = CommandLineParser.parse( [ 'does_not_exist', 'arg2' ] )
-      errors.must_include "Error: input data file \"does_not_exist\" not found."
+      File.expects(:exists?).with('input_filename').returns(false)
+      _, errors = CommandLineParser.parse( [ 'input_filename', 'output_filename' ] )
+      errors.must_include "Error: input data file \"input_filename\" not found."
     end
     it "returns an error if the input data file is not a file" do
       File.expects(:exists?).with('input_filename').returns(true)
       File.expects(:file?).with('input_filename').returns(false)
-      _, errors = CommandLineParser.parse( [ 'input_filename', 'arg2' ] )
+      _, errors = CommandLineParser.parse( [ 'input_filename', 'output_filename' ] )
       errors.must_include "Error: input data file \"input_filename\" is not a file."
     end
     it "returns an error if the output data file exists and is not a file" do
-      skip
+      File.expects(:exists?).with('input_filename').returns(true)
+      File.expects(:file?).with('input_filename').returns(true)
+      File.expects(:exists?).with('output_filename').returns(true)
+      File.expects(:file?).with('output_filename').returns(false)
+      _, errors = CommandLineParser.parse( [ 'input_filename', 'output_filename' ] )
+      errors.must_include "Error: existing output data file \"output_filename\" is not a file."
     end
   end
 end
