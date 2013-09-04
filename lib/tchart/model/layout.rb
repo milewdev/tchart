1f5195893ab0e401b7079b7746c1af207bfb25fb
milewgit <milewgit@gmail.com>
2013-09-04 16:15:15 -0400

    Remove superfluous comments.

--- lib/tchart/model/layout.rb
+++ lib/tchart/model/layout.rb
@@ -1,15 +1,15 @@
 module TChart
   class Layout
     
-    attr_accessor :x_axis_length              # 
-    attr_accessor :x_axis_label_width         # 
-    attr_accessor :x_axis_label_y_coordinate  # 
-    attr_accessor :x_axis_tick_x_coordinates  # 
-    attr_accessor :x_axis_tick_dates          # 
-    attr_accessor :y_axis_length              # 
-    attr_accessor :y_axis_label_width         # 
-    attr_accessor :y_axis_label_x_coordinate  # 
-    attr_accessor :y_axis_tick_y_coordinates  # 
+    attr_accessor :x_axis_length
+    attr_accessor :x_axis_label_width
+    attr_accessor :x_axis_label_y_coordinate
+    attr_accessor :x_axis_tick_x_coordinates
+    attr_accessor :x_axis_tick_dates
+    attr_accessor :y_axis_length
+    attr_accessor :y_axis_label_width
+    attr_accessor :y_axis_label_x_coordinate
+    attr_accessor :y_axis_tick_y_coordinates
     
     def date_range_to_x_coordinates(date_range) # => [ Numeric, Numeric ]
       x_from = date_to_x_coordinate(date_range.begin)
