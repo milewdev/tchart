16842b281c07b531b2d93d778578370a24c6948e
Michael Lewandowski <milewgit@gmail.com>
2013-09-03 23:07:14 -0400

    Use @attribute instead of #attribute.
    Still trying to decide which is the best way to go on this.

--- lib/tchart/model/y_item.rb
+++ lib/tchart/model/y_item.rb
@@ -18,18 +18,18 @@ module TChart
   private
   
     def new_y_label(layout, y) # => Label
-      Label.build_ylabel(xy(layout.y_axis_label_x_coordinate, y), layout.y_axis_label_width, description)
+      Label.build_ylabel(xy(layout.y_axis_label_x_coordinate, y), layout.y_axis_label_width, @description)
     end
     
     def new_bars(layout, y) # => [ Bar, Bar, ... ]
-      date_ranges.map do |date_range| 
+      @date_ranges.map do |date_range| 
         x_from, x_to = layout.date_range_to_x_coordinates(date_range)
         new_bar(x_from, x_to, y)
       end
     end
     
     def new_bar(x_from, x_to, y) # => Bar
-      Bar.new(xy(x_from, y), xy(x_to, y), bar_style)
+      Bar.new(xy(x_from, y), xy(x_to, y), @bar_style)
     end
     
   end
