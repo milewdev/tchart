65cfac2a955cae309b4f54bc12c00a1bd0ae01ca
Michael Lewandowski <milewgit@gmail.com>
2014-04-23 22:33:11 -0400

    Add coveralls test coverage.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'           # must be before everything else
+Coveralls.wear!
+
 require 'minitest/autorun'
 require 'mocha/setup'         # must be after require 'minitest/autorun'
 
